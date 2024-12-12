5c0323975e069972998194b978fa02914e70d0e9
dulcoon <michaelfebian353@gmail.com>
2024-12-12 14:59:26 +0700

    first commit

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,23 +1,26 @@
-import { gsap } from "gsap";
-
-gsap.to("#logo-container", { x: 300, duration: 1 });
-
-
 window.addEventListener("load", enableMobileMenu);
 window.addEventListener("resize", enableMobileMenu);
 
 function enableMobileMenu() {
   let menu = document.querySelector(".menu"),
-    links = document.querySelector(".links");
+    links = document.querySelector(".links"),
+    linkItems = document.querySelectorAll(".links a"); // Seleksi semua tautan dalam menu
+
   if (window.innerWidth <= 1024) {
     menu.addEventListener("click", () => {
       menu.classList.toggle("opened");
       links.classList.toggle("show");
+    });
+
+    // Tambahkan event listener untuk setiap tautan
+    linkItems.forEach((link) => {
+      link.addEventListener("click", () => {
+        if (menu.classList.contains("opened")) menu.classList.remove("opened");
+        if (links.classList.contains("show")) links.classList.remove("show");
+      });
     });
   } else {
     if (menu.classList.contains("opened")) menu.classList.remove("opened");
     if (links.classList.contains("show")) links.classList.remove("show");
   }
 }
-
-
